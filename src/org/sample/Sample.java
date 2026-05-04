3f94d583c862b8c68b3196d7b42fd2a28bb84225
sathya11042000 <sathyastudy2000@gmail.com>
2026-05-04 21:35:59 +0530

    h

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -3,11 +3,9 @@ package org.sample;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("java");
-<<<<<<< HEAD
-		System.out.println("sathya");
-=======
 		System.out.println("abi");
->>>>>>> 78de1784e15d4a94f19e6b6ecd16d75d25fb784d
+		System.out.println("sathya");
+
 	}
 
 }
